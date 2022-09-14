d59915d773a74c7eb8204dbd733e8b31b873813a
unknowncoderboy <sammyphonist@gmail.com>
2022-09-13 20:05:02 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,13 @@
  * prints _putchar using putchar prototype
  *
  * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
 	char str[] = "_putchar";
 	int ch;
+
 	for (ch = 0; ch < 8; ++ch)
 	{
 		_putchar(str[ch]);
